1b3016f5e1fe953f4e8847ce8a3b5905cc6951bf
mzaini30 <muhzaini30@gmail.com>
2019-02-18 23:51:24 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -2,8 +2,16 @@ $('.navbar-nav a').click(function(){
 	$('.navbar-toggle').click()
 })
 
+$('.nama').val(localStorage.getItem('nama'))
+$('.judul-buku').val(localStorage.getItem('judul-buku'))
+$('.genre').val(localStorage.getItem('genre'))
+$('.premis').val(localStorage.getItem('premis'))
+
 $('.kirim').click(function(){
-	
+	localStorage.setItem('nama', $('.nama').val())
+	localStorage.setItem('judul-buku', $('.judul-buku').val())
+	localStorage.setItem('genre', $('.genre').val())
+	localStorage.setItem('premis', $('.premis').val())
 })
 
 // autocomplete(document.getElementById("myInput"), countries);
