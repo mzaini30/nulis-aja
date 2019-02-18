8cba318a3414fed93bec82e5bbb976ac61b3e417
mzaini30 <muhzaini30@gmail.com>
2019-02-18 23:32:59 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@ $('.navbar-nav a').click(function(){
 	$('.navbar-toggle').click()
 })
 
+$('.kirim').click(function(){
+	
+})
+
 // autocomplete(document.getElementById("myInput"), countries);
 autocomplete(document.getElementById('nama'), nama)
 autocomplete(document.getElementById('genre'), genre)
