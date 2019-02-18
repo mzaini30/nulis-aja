b0f7242d3d9c646b9662710094a7b80ec5ebab6c
mzaini30 <muhzaini30@gmail.com>
2019-02-19 00:17:54 +0800

    oke

--- app/app.js
+++ app/app.js
@@ -2,6 +2,10 @@ $('.navbar-nav a').click(function(){
 	$('.navbar-toggle').click()
 })
 
+if (localStorage.getItem('jumlah-halaman') === null){
+	localStorage.setItem('jumlah-halaman', '[]')
+}
+
 $('.nama').val(localStorage.getItem('nama'))
 $('.judul-buku').val(localStorage.getItem('judul-buku'))
 $('.genre').val(localStorage.getItem('genre'))
@@ -12,6 +16,10 @@ $('.kirim').click(function(){
 	localStorage.setItem('judul-buku', $('.judul-buku').val())
 	localStorage.setItem('genre', $('.genre').val())
 	localStorage.setItem('premis', $('.premis').val())
+	data_jumlah_halaman = JSON.parse(localStorage.getItem('jumlah-halaman'))
+	data_jumlah_halaman.push($('.jumlah-halaman').val())
+	localStorage.setItem('jumlah-halaman', JSON.stringify(data_jumlah_halaman))
+	// location.reload()
 })
 
 // autocomplete(document.getElementById("myInput"), countries);
