fe6c6f2998e8359955dc891a7a002c2aa0effc6c
mzaini30 <muhzaini30@gmail.com>
2019-02-22 06:21:01 +0800

    upload blog

---
+++ vendor/simple-chart/simple-chart.js
@@ -0,0 +1,10 @@
+simple_chart = function(div, data){
+	// $(div).html('tes')
+	$(div).css({
+		width: 'auto',
+		height: '300px'
+	})
+	jumlah_data = data.data.length // 6
+	// $('title').html(jumlah_data)
+	
+}
--- versi 1/app/app.js
+++ versi 1/app/app.js
@@ -26,33 +26,51 @@ update_persen = function(){
 
 grafik = function(){
 	data_grafik = JSON.parse(localStorage.getItem('jumlah-halaman')).reverse()
-	chart = {
-		data: [
-			data_grafik[5],
-        	data_grafik[4],
-        	data_grafik[3],
-        	data_grafik[2],
-        	data_grafik[1],
-        	data_grafik[0]
-		],
-		background: [
-			'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-		],
-		color: [
-			'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-		]
-	}
-	simple_chart('.chart', chart)
+	var ctx = document.getElementById("myChart").getContext('2d');
+	var myChart = new Chart(ctx, {
+	    type: 'bar',
+	    data: {
+	        labels: ["", "", "", "", "", ""],
+	        datasets: [{
+	            label: 'Jumlah Halaman',
+	            // data: [12, 19, 3, 5, 2, 3],
+	            data: [
+	            	data_grafik[5],
+	            	data_grafik[4],
+	            	data_grafik[3],
+	            	data_grafik[2],
+	            	data_grafik[1],
+	            	data_grafik[0]
+	            ],
+	            backgroundColor: [
+	                'rgba(255, 99, 132, 0.2)',
+	                'rgba(54, 162, 235, 0.2)',
+	                'rgba(255, 206, 86, 0.2)',
+	                'rgba(75, 192, 192, 0.2)',
+	                'rgba(153, 102, 255, 0.2)',
+	                'rgba(255, 159, 64, 0.2)'
+	            ],
+	            borderColor: [
+	                'rgba(255,99,132,1)',
+	                'rgba(54, 162, 235, 1)',
+	                'rgba(255, 206, 86, 1)',
+	                'rgba(75, 192, 192, 1)',
+	                'rgba(153, 102, 255, 1)',
+	                'rgba(255, 159, 64, 1)'
+	            ],
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
 
 	if (data_grafik[0] != null){
 		$('.angka').html(Math.max(...data_grafik.map(Number)))
@@ -98,5 +116,5 @@ $('.kirim').click(function(){
 })
 
 // autocomplete(document.getElementById("myInput"), countries);
-// autocomplete(document.getElementById('nama'), nama)
-// autocomplete(document.getElementById('genre'), genre)
+autocomplete(document.getElementById('nama'), nama)
+autocomplete(document.getElementById('genre'), genre)
